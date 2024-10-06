705a79e8669670e7c56e3caee35c3ae89f39be52
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-10-06 05:15:44 -0400

    t   - created test with generated message

--- Calculator/test/com/example/CalculatorTest.java
+++ Calculator/test/com/example/CalculatorTest.java
@@ -10,6 +10,18 @@ class CalculatorTest {
     void integerDivision() {
         Calculator calculator = new Calculator();
         int result = calculator.integerDivision(4,2);
-        assertEquals(2,result);
+        assertEquals(2, result, "4/2 dot not produce 2");
+    }
+
+    @Test
+    void integerSubtraction() {
+        Calculator calculator = new Calculator();
+        int minuend = 33;
+        int subtrahend = 1;
+        int expectedResult = 32;
+
+        int actualResult = calculator.integerSubtraction(33,1);
+        assertEquals(expectedResult, actualResult,
+                minuend + " - " + subtrahend + " did not produce " + expectedResult);
     }
 }
