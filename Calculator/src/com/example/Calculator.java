869abc7a4d61f951f3ca38208c22dba6e867a912
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-10-05 21:45:59 -0400

    t   - created first unit test

--- Calculator/src/com/example/Calculator.java
+++ Calculator/src/com/example/Calculator.java
@@ -3,6 +3,6 @@ package com.example;
 public class Calculator {
 
     public int integerDivision(int dividend, int divisor) {
-        return dividend/divisor;
+        return dividend / divisor;
     }
 }
