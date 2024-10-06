d909e2684645aa9d6a72ccf2e5d94838830f6f94
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-10-06 05:54:41 -0400

    t   - updated test to follow AAA

--- Calculator/test/com/example/CalculatorTest.java
+++ Calculator/test/com/example/CalculatorTest.java
@@ -11,9 +11,17 @@ class CalculatorTest {
     @DisplayName("Test 4/2 = 2")
     @Test
     void testIntegerDivision_WhenFourIsDividedByTwo_ShouldReturnTwo() {
+        // Arrange
         Calculator calculator = new Calculator();
-        int result = calculator.integerDivision(4,2);
-        assertEquals(2, result, "4/2 dot not produce 2");
+        int dividend = 4;
+        int divisor = 2;
+        int expectedResult = 2;
+
+        // Act
+        int actualResult = calculator.integerDivision(dividend, divisor);
+
+        // Assert
+        assertEquals(expectedResult, actualResult, "4/2 dot not produce 2");
     }
 
     @DisplayName("Test division by zero")
