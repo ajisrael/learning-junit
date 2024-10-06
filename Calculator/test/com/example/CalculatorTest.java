efe216c2419297baa3987ec7b9ec947dcabe6c96
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-10-06 05:42:15 -0400

    t   - added display names to tests to make report easier to read

--- Calculator/test/com/example/CalculatorTest.java
+++ Calculator/test/com/example/CalculatorTest.java
@@ -1,11 +1,14 @@
 package com.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test Math operations in Calculator class")
 class CalculatorTest {
 
+    @DisplayName("Test 4/2 = 2")
     @Test
     void testIntegerDivision_WhenFourIsDividedByTwo_ShouldReturnTwo() {
         Calculator calculator = new Calculator();
@@ -13,6 +16,13 @@ class CalculatorTest {
         assertEquals(2, result, "4/2 dot not produce 2");
     }
 
+    @DisplayName("Test division by zero")
+    @Test
+    void testIntegerDivision_WhenDividendIsDividedByZero_ShouldThrowArithmeticException() {
+        fail("Not implemented yet");
+    }
+
+    @DisplayName("Test 33-1 = 32")
     @Test
     void integerSubtraction() {
         Calculator calculator = new Calculator();
