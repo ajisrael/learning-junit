b33b26071d792f48975c956eb96e1657541c5d80
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-10-07 22:08:48 -0400

    t   - changed test lifecycle to be per class

--- Calculator/test/com/example/MethodOrderedByOrderIndexTest.java
+++ Calculator/test/com/example/MethodOrderedByOrderIndexTest.java
@@ -1,34 +1,43 @@
 package com.example;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodOrderedByOrderIndexTest {
+
+    StringBuilder completed = new StringBuilder("");
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("State of instance object: " + completed);
+    }
 
     @Order(1)
     @Test
     void testD() {
         System.out.println("Running test D");
+        completed.append("1");
     }
 
     @Order(2)
     @Test
     void testA() {
         System.out.println("Running test A");
+        completed.append("2");
     }
 
     @Order(3)
     @Test
     void testC() {
         System.out.println("Running test C");
+        completed.append("3");
     }
 
     @Order(4)
     @Test
     void testB() {
         System.out.println("Running test B");
+        completed.append("4");
     }
 }
