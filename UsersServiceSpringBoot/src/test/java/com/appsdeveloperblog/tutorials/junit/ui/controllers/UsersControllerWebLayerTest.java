bcb0b0719648f08f88c6a308dafb851c32010f5d
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2024-10-09 23:57:38 -0400

    t   - created integration test for create user post endpoint

--- UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWebLayerTest.java
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWebLayerTest.java
@@ -1,12 +1,79 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import com.appsdeveloperblog.tutorials.junit.service.UsersService;
+import com.appsdeveloperblog.tutorials.junit.service.UsersServiceImpl;
+import com.appsdeveloperblog.tutorials.junit.shared.UserDto;
+import com.appsdeveloperblog.tutorials.junit.ui.request.UserDetailsRequestModel;
+import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @WebMvcTest(controllers = UsersController.class,
 excludeAutoConfiguration = {SecurityAutoConfiguration.class})
 //@AutoConfigureMockMvc(addFilters = false)
+//@MockBean({UsersServiceImpl.class})
 public class UsersControllerWebLayerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    // @Autowired
+    @MockBean
+    UsersService usersService;
+
+    @Test
+    @DisplayName("User can be created")
+    void testCreateUser_whenValidUserDetailsProvided_returnsCreatedUserDetials() throws Exception {
+        // Arrange
+        UserDetailsRequestModel userDetailsRequestModel = new UserDetailsRequestModel();
+        userDetailsRequestModel.setFirstName("Sergey");
+        userDetailsRequestModel.setLastName("Kargopolov");
+        userDetailsRequestModel.setEmail("test@test.com");
+        userDetailsRequestModel.setPassword("12345678");
+        userDetailsRequestModel.setPassword("12345678");
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(userDetailsRequestModel));
+
+        UserDto userDto = new UserDto();
+        userDto.setFirstName("Sergey");
+        userDto.setLastName("Kargopolov");
+        userDto.setEmail("test@test.com");
+        userDto.setUserId(UUID.randomUUID().toString());
+
+        when(usersService.createUser(any(UserDto.class))).thenReturn(userDto);
+
+        // Act
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+        String responseBodyAsString = mvcResult.getResponse().getContentAsString();
+        UserRest createdUser = new ObjectMapper().readValue(responseBodyAsString, UserRest.class);
+
+        // Assert
+        assertEquals(userDetailsRequestModel.getFirstName(), createdUser.getFirstName(), "First names do not match");
+        assertEquals(userDetailsRequestModel.getLastName(), createdUser.getLastName(), "Last names do not match");
+        assertEquals(userDetailsRequestModel.getEmail(), createdUser.getEmail(), "Emails do not match");
+        assertFalse(createdUser.getUserId().isEmpty(), "User id should not be empty");
+    }
 }
