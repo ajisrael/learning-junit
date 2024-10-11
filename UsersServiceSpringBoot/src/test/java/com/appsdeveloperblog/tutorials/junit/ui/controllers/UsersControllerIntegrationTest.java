6dd17842079b8e61e67cce20b8675b879c7af97d
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2024-10-11 09:34:36 -0400

    t   - wip test

--- UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,22 +1,27 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UsersControllerIntegrationTest {
 
-    @Value("${server.port}")
-    private int serverPort;
-
-    @LocalServerPort
-    private int localServerPort;
-
     @Test
-    void contextLoads() {
-        System.out.println("Server port: " + serverPort);
-        System.out.println("Local server port: " + localServerPort);
+    @DisplayName("User can be created")
+    void testCreateUser_whenValidDetailsProvided_returnsUserDetails() {
+        // Arrange
+//        String createUserJson = "{\n" +
+//                "   \"firstName\":\"Sergey\",\n" +
+//                "   \"lastName\":\"Kargopolov\",\n" +
+//                "   \"email\":\"test@test.com\",\n" +
+//                "   \"password\":\"12345678\",\n" +
+//                "   \"repeatPassword\":\"12345678\",\n" +
+//                "}";
+
+        // Act
+
+        // Assert
+
     }
 }
